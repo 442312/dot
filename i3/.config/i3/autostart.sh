9dd0e7c40534307b2030eb73c8c4d2abd1536f84
442312 <1768438@gmail.com>
2020-03-03 00:28:54 +0300

    add i3

---
+++ i3/.config/i3/autostart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+lxpolkit &
+hsetroot -solid "#353535" &
+nitrogen --restore &
+nm-applet &
+(sleep 3s && clipit) &
+mate-volume-control-applet &
+xfce4-power-manager &
+thunar --daemon &
+xset r rate 250 25 &
+xset b off &
+conky -q &
+xss-lock -l blurlock &
+setxkbmap -layout 'us,ru' -option 'grp:caps_toggle' &
+fbxkb &
+#cbpp-compositor --start &
+#xscreensaver -no-splash &
